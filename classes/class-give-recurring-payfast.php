2c9447cfdfd8f3c3d9720dae65924064db2e47f4
Demonix <demonix77x@gmail.com>
2018-08-03 15:00:49 +0200

    Class Fixes

--- classes/class-give-recurring-payfast.php
+++ classes/class-give-recurring-payfast.php
@@ -110,7 +110,9 @@ class Give_Recurring_PayFast extends Give_Recurring_Gateway {
 				$period = isset( $price['_give_period'] ) ? $price['_give_period'] : 0;
 
 				if ( in_array( $period, array( 'day', 'week' ) ) ) {
-					wp_die( esc_html( $message ), __( 'Error', 'give-recurring' ), array( 'response' => 400 ) );
+					wp_die( esc_html( $message ), esc_html__( 'Error', 'give-recurring' ), array( 
+						'response' => 400 
+					) );
 				}
 			}
 		} elseif ( Give_Recurring()->is_recurring( $form_id ) ) {
@@ -118,7 +120,7 @@ class Give_Recurring_PayFast extends Give_Recurring_Gateway {
 			$period = isset( $_REQUEST['_give_period'] ) ? $_REQUEST['_give_period'] : 0;
 
 			if ( in_array( $period, array( 'day', 'week' ) ) ) {
-				wp_die( esc_html( $message) , __( 'Error', 'give-recurring' ), array(
+				wp_die( esc_html( $message ) , esc_html__( 'Error', 'give-recurring' ), array(
 					'response' => 400,
 				) );
 			}
